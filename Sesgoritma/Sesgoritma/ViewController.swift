4d116563c202b5e5c998318cd2fe14ae3b78df22
Arda Mavi <ardamavi2@gmail.com>
2018-03-30 18:02:06 +0300

    Change camera position(Front-Back) button added

--- Sesgoritma/Sesgoritma/ViewController.swift
+++ Sesgoritma/Sesgoritma/ViewController.swift
@@ -13,9 +13,10 @@ import AVFoundation
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    let captureSession = AVCaptureSession()
+    var captureSession = AVCaptureSession()
     let synth = AVSpeechSynthesizer()
-    var utterance = AVSpeechUtterance(string: "")
+    var cameraPos = AVCaptureDevice.Position.back
+    var captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: AVCaptureDevice.Position.back)
     var old_char = ""
     
     @IBOutlet var predictLabel: UILabel!
@@ -23,6 +24,18 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         captureSession.stopRunning()
         synth.stopSpeaking(at: AVSpeechBoundary.immediate)
         UIApplication.shared.isIdleTimerDisabled = false
+    }
+    @IBAction func change_camera(_ sender: Any) {
+        captureSession.stopRunning()
+        synth.stopSpeaking(at: AVSpeechBoundary.immediate)
+        if cameraPos == AVCaptureDevice.Position.back{
+            cameraPos = AVCaptureDevice.Position.front
+        }else{
+            cameraPos = AVCaptureDevice.Position.back
+        }
+        captureSession = AVCaptureSession()
+        view.layer.sublayers?[0].removeFromSuperlayer()
+        self.viewDidLoad()
     }
     
     override func viewDidLoad() {
@@ -36,9 +49,9 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         captureSession.sessionPreset = .photo
         
-        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {return}
+        self.captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: cameraPos)
         
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice!) else {return}
         captureSession.addInput(input)
         
         captureSession.startRunning()
@@ -77,10 +90,10 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
                     
                     self.predictLabel.text =  String(firstObservation.identifier)
                     
-                    self.utterance = AVSpeechUtterance(string: String(firstObservation.identifier))
-                    self.utterance.voice = AVSpeechSynthesisVoice(language: AVSpeechSynthesisVoice.currentLanguageCode())
-                    self.synth.stopSpeaking(at: AVSpeechBoundary.immediate)
-                    self.synth.speak(self.utterance) // For mute the previous speak.
+                    let utterance = AVSpeechUtterance(string: String(firstObservation.identifier))
+                    utterance.voice = AVSpeechSynthesisVoice(language: AVSpeechSynthesisVoice.currentLanguageCode())
+                    self.synth.stopSpeaking(at: AVSpeechBoundary.immediate) // For mute the previous speak.
+                    self.synth.speak(utterance)
                     self.old_char = String(firstObservation.identifier)
                     
                 }
